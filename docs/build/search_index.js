21846604d28e4faf5cb6c7dbd32e67aba4e26a1b
rodrigo <rodrigovp2112@gmail.com>
2026-07-19 04:34:37 -0300

    local

---
+++ docs/build/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"category":"section","location":"","page":"Inicio","text":"index","title":"Inicio"},{"category":"section","location":"01_modelado/","page":"Modelado de Sistemas","text":"Para definir una función de transferencia de un sistema masa-resorte-amortiguador genérico, usamos el paquete ControlSystemsBase.\n\nusing ControlSystems\n\n# Definimos variables\nm = 1.0  # Masa\nb = 0.2  # Amortiguamiento\nk = 5.0  # Constante del resorte\n\n# Creamos la función de transferencia\nG = tf([1], [m, b, k])","title":"Modelado de Sistemas"}]
+}
